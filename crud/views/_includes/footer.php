82b0ff85d4c5c7881c9d4a2b732920c327c06f3f
BluFanton <rborgesdonascimento@gmail.com>
2016-06-29 21:02:51 -0300

    Alteração com footer HTML

--- crud/views/_includes/footer.php
+++ crud/views/_includes/footer.php
@@ -1,6 +1,19 @@
 <?php if ( ! defined('ABSPATH')) exit; ?>
 
+
+<footer>
+    <p><strong>Academia Borybuilder de Franca</strong><br />
+    Rua Sem Flores, 1313 &ndash; Vila Santa Rita<br />
+    Franca/SP &ndash; CEP 14405-131<br />
+    Telefone: (13) 3713-1313 &ndash; Fax: 3713-1313<br />
+    <a href="mailto:bodybuilder@gmail.com">bodybuilder@gmail.com</a> &ndash;
+      <a href="http://bodybuilder.com.br">bodybuilder.com.br</a>
+    </p>
+    <p>&copy; Copyright 2016&ndash; <a href="mailto:fabio.f.castro@outlook.com">Fabio Castro e Rafael Borges</a>. Todos os direitos reservados.</p>
+</footer>
+
 </div> <!-- .main-page (header.php) -->
 
 </body>
+
 </html>
--- crud/views/home/home-view.php
+++ crud/views/home/home-view.php
@@ -2,10 +2,9 @@
 
 
 <div class="wrap">
-
 	<p>Olá, <br>
 	Se você está visualizando esta página, seu sistema PHP com padrão MVC está funcionando.</p>
 
 	<p>Não deixe de visitar nosso site para dicas <a href="http://www.tutsup.com">tutsup.com</a>.
+</div><!-- .wrap -->
 
-</div> <!-- .wrap -->
